32329e48d05885a69204b1e2f7f538cd1d5dade0
Kofku <ewe727@gmail.com>
2024-04-10 13:36:54 +0100

    The project

---
+++ GameStore.Api/Data/GameStoreContex.cs
@@ -0,0 +1,20 @@
+﻿using GameStore.Api.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace GameStore.Api.Data;
+
+public class GameStoreContext(DbContextOptions<GameStoreContext> options) : DbContext(options)
+{
+    public DbSet<Game> Games => Set<Game>();
+
+    public DbSet<Genre> Genres => Set<Genre>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Genre>().HasData(
+            new { Id = 1, Name = "RPG" },
+            new { Id = 2, Name = "Indie" },
+            new { Id = 3, Name = "Puzzle" }
+        );
+    }
+}
